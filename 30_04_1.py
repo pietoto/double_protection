b247444a7b21c8616364b794aba6341002ca1381
pietoto <tanya.vostrikova.2006@mail.ru>
2023-04-30 01:15:17 +0400

    ---
    ---

---
+++ 30_04_1.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect
+
+from loginform import LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    app.run()
